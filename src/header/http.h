a4d555ccaaed86fadbd6c812f8fa7c08e2210efd
wuhuaji0 <wuhuaji0@gmail.com>
2019-08-07 22:52:59 +0800

    精简请求包定义

--- src/header/http.h
+++ src/header/http.h
@@ -1,31 +1,24 @@
 //
 // Created by 吴化吉 on 2019-08-05.
 //
-
-
-/*http请求行*/
-typedef struct {
-    char *method;
-    char *uri;
-    char *protocol;
-} request_line;
-
-/*请求头*/
-typedef struct {
-    char *host;
-    char *connection;
-    char *useragent;
-    char *accept;
-    char *acceptencoding;
-    /* 先读取基本字段，更多的的之后再读 */
-} request_header;
-
-
 /*请求包*/
 typedef struct {
-    request_line req_line;
-    request_header req_header;
+    struct request_line{
+        char *method;
+        char *uri;
+        char *protocol;
+    } req_line;
+
+    struct request_header{
+        char *host;
+        char *connection;
+        char *useragent;
+        char *accept;
+        char *acceptencoding;
+        /* 先读取基本字段，更多的的之后再读 */
+    } req_header;
 } request_package;
+
 
 
 int parse_request(int client);
