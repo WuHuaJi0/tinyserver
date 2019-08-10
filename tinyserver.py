acc7fa555c6da016428d120ca6dc06ba81cc3070
wuhuaji0 <wuhuaji0@gmail.com>
2019-08-10 23:45:31 +0800

    python版：读取静态文件并返回

--- tinyserver.py
+++ tinyserver.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     print("监听在："+ str(port) +" 端口")
     server_socket = tcp.server_listen(port)
     while True:
-        server_socket.accept()
         (client_socket, address) = server_socket.accept()
 
         # todo: 这里假定1000个字符把http请求头读取完了，实际上可能没有；
