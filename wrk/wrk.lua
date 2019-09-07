6d5dd0beb040a63d8f2206495942b1176e0b5776
wuhuaji0 <wuhuaji0@gmail.com>
2019-09-07 15:45:53 +0800

    wrk测试脚本

---
+++ wrk/wrk.lua
@@ -0,0 +1,15 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wuhuaji
+-- Date: 9/7/19
+-- Time: 3:42 PM
+-- To change this template use File | Settings | File Templates.
+-- wrk测试运行脚本
+--
+
+function response(status,headers,body)
+    print(status)
+    print(headers)
+    print(body)
+end
+
